e99c61cb23e9c49a2c1f63f2d66266cb97fc7464
tmoynat <thibault.moynat@epitech.eu>
2019-08-07 15:04:54 +0200

    [create] ungzip function

---
+++ gzip.go
@@ -0,0 +1,42 @@
+package go_gzip
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+)
+
+func check_err(err error, msg string) {
+	if err != nil {
+		fmt.Println("Error : ", msg, " failed...")
+	}
+}
+
+func ungzipator(gzipstream io.Reader) {
+	uncompressed, err := gzip.NewReader(gzipstream)
+	check_err(err, "uncompression")
+	tarstream := tar.NewReader(uncompressed)
+	for true {
+		header, err := tarstream.Next()
+		if err == io.EOF {
+			break
+		}
+		check_err(err, "iteration on files")
+		switch header.Typeflag {
+		case tar.TypeDir:
+			err := os.Mkdir(header.Name, 0755)
+			check_err(err, "folder creation")
+		case tar.TypeReg:
+			outFile, err := os.Create(header.Name)
+			check_err(err, "file creation")
+			defer outFile.Close()
+			bytes, err := io.Copy(outFile, tarstream)
+			check_err(err, "copy")
+			fmt.Println(bytes, " bytes written in : ", header.Name)
+		default:
+			fmt.Println("Error : unknown type: %s in %s", header.Typeflag, header.Name)
+		}
+	}
+}
